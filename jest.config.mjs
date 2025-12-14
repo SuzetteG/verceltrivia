fb13541a262076e1002a49a4e2f03e350535cee0
Suzette Gorrell <tayloresuzette@gmail.com>
2025-12-14 13:59:42 -0700

    Update configs and lockfiles after troubleshooting CI and test setup

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,7 +1,7 @@
 // type: module
 export default {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
   moduleFileExtensions: ['js', 'jsx'],
   transform: {
     '^.+\\.(js|jsx)$': 'babel-jest',
